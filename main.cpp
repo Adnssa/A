e2025040942065601e6ccae310cfb1e5c78c4ae5
Daniel Palacios Corral <dpalacios@asmws.com>
2019-05-12 14:19:40 +0200

    mig

--- main.cpp
+++ main.cpp
@@ -8,40 +8,74 @@
 
 using namespace std;
 
+
 /* vector<vector<int>> graph; */
 
-void fillRow(int** gp, int n, int row) {
+ vector<vector<int>> fillRow(vector<vector<int>> gp, int n, int row) {
     for (int i = 0; i < n; i++) {
     	gp[row][i] = 0;
     }
+    return gp;
 }
 
-void fillCol(int** gp, int n, int col) {
+vector<vector<int>> fillCol(vector<vector<int>> gp, int n, int col) {
     for (int i = 0; i < n; i++) {
     	gp[i][col] = 0;
     }
+    return gp;
 }
 
-int** getNewGraph(int* gp, int n, float prov) {
+vector<vector<int>> getNewGraph(vector<vector<int>> gp, int n, float prov) {
+    vector<vector<int>> ngp = gp;
     for (int i = 0; i < n; i++) {
 		float pi = (float) rand()/RAND_MAX;
         if (pi < prov) {
         	cout << "DELETED VERTEX" << i << endl;
-            fillRow(gp, n, i);
-            fillCol(gp, n, i);
+            ngp = fillRow(ngp, n, i);
+            ngp = fillCol(ngp, n, i);
         } else {
             for (int j = 0; j < n; j++) {
+                if (ngp[i][j] == 0) continue;  
                 float pj = (float) rand()/RAND_MAX;
                 if (pj < prov) {
                 	cout << "DELETED EDGE I: " << i << " J: " << j << endl;
                     /* int aux = gp[i][j]; */
-                    gp[i][j] = 0;
-                    gp[j][i] = 0;
+                    ngp[i][j] = 0;
+                    ngp[j][i] = 0;
                 }
             }
         }
     }
-    return gp;
+    return ngp;
+}
+
+// GENERATES GRAELLA N X N
+vector<vector<int>> genGraellaN(int n) {
+    int nr = n*n;
+    int lastRow = (nr - n);
+    vector<vector<int>> graella;
+    graella.resize(nr, vector<int>(nr, 0));
+    for (int i = 0; i < nr; i++) {
+        if (i%n != (n-1)) {
+            cout << i << " LAST COL" << endl; 
+            graella[i][i+1] = 1;
+            graella[i+1][i] = 1;
+        }
+
+        if (i < lastRow) {
+            cout << i << " LAST ROW" << endl; 
+            graella[i][i+n] = 1;
+            graella[i+n][i] = 1;
+        }
+    }
+    return graella;
+}
+
+vector<int> unFind(vector<vector<int>> graella, int n) {
+    int nr = n*n;
+    vector<int> result;
+    result.resize(nr, -1);
+
 }
 
 int main() 
@@ -63,8 +97,11 @@ int main()
 	std::string line;
  //    cout << "N: " << n << endl;
     
-    int graph[n][n] = {};
-    fill(*graph, *graph + n*n , 0);
+    // int graph[n][n] = {};
+    vector<vector<int>> graph;
+    graph.resize(n, vector<int>(n, 0));
+
+    //fill(*graph, *graph + n*n , 0);
     int i = 0;
 
     
@@ -103,16 +140,34 @@ int main()
 
     /* cout << graph.size(); */
 
-    cout << "HOLA 2"<< endl;
+    cout << "ORIGINAL GRAPH"<< endl;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             cout << "(I, J): (" << i << "," << j << ") = " << graph[i][j] << endl;
         }
     }
 
-    graph = getNewGraph(&graph, n, 0.5); 
+    vector<vector<int>> ngraph;
+
+    ngraph = getNewGraph(graph, n, 0.2); 
+
+    cout << "NEW GRAPH"<< endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << "(I, J): (" << i << "," << j << ") = " << ngraph[i][j] << endl;
+        }
+    }
+
+    // vector<vector<int>> ngraph;
+    ngraph = genGraellaN(5); 
+
+    cout << "GRAELLA"<< endl;
+    for (int i = 0; i < ngraph.size(); i++) {
+        for (int j = 0; j < ngraph.size(); j++) {
+            cout << "(I, J): (" << i << "," << j << ") = " << ngraph[i][j] << endl;
+        }
+    }
+
+
     return 0;
 }
-
-
-
